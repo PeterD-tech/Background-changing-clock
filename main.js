90192c42cc0c22c4f86576d82b9f902369498da9
Peter Durovka <durovka.peter@gmail.com>
2020-11-15 17:03:59 +0100

    added color switching to body

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const clock = document.getElementById('clock');
+let i = 0;
 setInterval(() => {
     let time = new Date();
     clock.innerText = time.toTimeString().slice(0,8);
-    
 }, 1000);
+setInterval(() => {
+    let color = `hsla(${i}, 100%, 50%,1)`;
+    document.body.style.background = color;
+    i += 1 ;
+}, 100);
